4be632920d1d555b0dad92b70ca352c57ae20539
[yousincha] <[yousincha@naver.com]>
2023-10-18 16:51:12 +0900

    20231018-3 commit

---
+++ scaling-up-with-reducer-and-context/src/App.js
@@ -0,0 +1,14 @@
+// reducer와 context 결합하기
+import AddTask from "./AddTask.js";
+import TaskList from "./TaskList.js";
+import { TasksProvider } from "./TasksContext.js";
+
+export default function TaskApp() {
+  return (
+    <TasksProvider>
+      <h1>Day off in Kyoto</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
+  );
+}
